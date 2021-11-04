86bb083bdcd7ce5cca7fea6b1774de60e1f1767c
Delvoid <delvoidltd@gmail.com>
2021-11-04 21:06:26 +0000

    passing course changes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,24 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require('chai').expect
+const ConvertHandler = require('../controllers/convertHandler.js')
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+  let convertHandler = new ConvertHandler()
 
-};
+  app.route('/api/convert').get((req, res) => {
+    const input = req.query.input
+    const initNum = convertHandler.getNum(input)
+    const initUnit = convertHandler.getUnit(input)
+
+    if (!initNum && !initUnit) res.json('invalid number and unit')
+    if (!initNum) res.json('invalid number')
+    if (!initUnit) res.json('invalid unit')
+
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+
+    res.json({ initNum, initUnit, returnNum, returnUnit, string: toString })
+  })
+}
